746e60ce8e79a2984a042102817acd1a80e4e9e3
Red Hat VDO Team <vdo-devel@redhat.com>
2019-01-02 04:38:39 -0500

    137691 by sweettea on 2019/01/02 (pair: joe) [VDOUP-3] Add file missing from 137690.
    .../dataVIO.c
    Fixed call to go through copyData() instead of layer->copyData().
    
    Co-authored-by: sweettea <sweettea@redhat.com>
    Co-authored-by: joe <jochapma@redhat.com>

--- vdo/base/dataVIO.c
+++ vdo/base/dataVIO.c
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301, USA. 
  *
- * $Id: //eng/linux-vdo/src/c++/vdo/base/dataVIO.c#1 $
+ * $Id: //eng/linux-vdo/src/c++/vdo/base/dataVIO.c#2 $
  */
 
 #include "dataVIO.h"
@@ -265,7 +265,7 @@ void attemptLogicalBlockLock(VDOCompletion *completion)
    * allocation, prevent it from blocking in the packer and wait on it.
    */
   if (isReadDataVIO(dataVIO) && atomicLoadBool(&lockHolder->hasAllocation)) {
-    dataVIOAsCompletion(dataVIO)->layer->copyData(lockHolder, dataVIO);
+    copyData(lockHolder, dataVIO);
     finishDataVIO(dataVIO, VDO_SUCCESS);
     return;
   }
