267ae4253401d27f0c8bbd1e0e466c153eec425a
Red Hat VDO Team <vdo-devel@redhat.com>
2019-01-15 04:56:08 -0500

    137819 by corwin on 2019/01/15 (pair: sweettea) Merged 137725 to linux-vdo. 137725 by corwin on 2019/01/03 (pair: sclafani) Removed readOnlyNotifierInternals.h [VDO-4402]
    .../readOnlyNotifierInternals.h
    .../readOnlyNotifier.c
    Moved contents of readOnlyNotifierInternals.h into readOnlyNotifier.c
    since no other file includes it; removed readOnlyNotifierInternals.h.
    
    Co-authored-by: corwin <corwin@redhat.com>
    Co-authored-by: sweettea <sweettea@redhat.com>
    Co-authored-by: sclafani <sclafani@redhat.com>

--- vdo/base/readOnlyNotifier.c
+++ vdo/base/readOnlyNotifier.c
@@ -16,11 +16,10 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301, USA. 
  *
- * $Id: //eng/linux-vdo/src/c++/vdo/base/readOnlyNotifier.c#1 $
+ * $Id: //eng/linux-vdo/src/c++/vdo/base/readOnlyNotifier.c#2 $
  */
 
 #include "readOnlyNotifier.h"
-#include "readOnlyNotifierInternals.h"
 
 #include "logger.h"
 #include "memoryAlloc.h"
@@ -29,6 +28,62 @@
 #include "completion.h"
 #include "physicalLayer.h"
 #include "threadConfig.h"
+
+/**
+ * An object to be notified when the VDO enters read-only mode
+ **/
+typedef struct readOnlyListener ReadOnlyListener;
+
+struct readOnlyListener {
+  /** The listener */
+  void                 *listener;
+  /** The method to call to notifiy the listener */
+  ReadOnlyNotification *notify;
+  /** A pointer to the next listener */
+  ReadOnlyListener     *next;
+};
+
+/**
+ * Data associated with each base code thread.
+ **/
+typedef struct threadData {
+  /** The completion for entering read-only mode */
+  VDOCompletion          completion;
+  /** The ReadOnlyNotifier to which this structure belongs */
+  ReadOnlyNotifier      *notifier;
+  /** The thread this represents */
+  ThreadID               threadID;
+  /** Whether this thread is in read-only mode */
+  bool                   isReadOnly;
+  /** Whether this thread is entering read-only mode */
+  bool                   isEnteringReadOnlyMode;
+  /** Whether this thread may enter read-only mode */
+  bool                   mayEnterReadOnlyMode;
+  /** The error code for entering read-only mode */
+  int                    readOnlyError;
+  /** A completion to notify when this thread is not entering read-only mode */
+  VDOCompletion         *superBlockIdleWaiter;
+  /** A completion which is waiting to enter read-only mode */
+  VDOCompletion         *readOnlyModeWaiter;
+  /**
+   * A list of objects waiting to be notified on this thread that the VDO has
+   * entered read-only mode.
+   **/
+  ReadOnlyListener      *listeners;
+} ThreadData;
+
+struct readOnlyNotifier {
+  /** The completion for waiting until not entering read-only mode */
+  VDOCompletion       completion;
+  /** Whether the listener has already started a notification */
+  bool                isReadOnly;
+  /** The thread config of the VDO */
+  const ThreadConfig *threadConfig;
+  /** The ID of the admin thread */
+  ThreadID            adminThreadID;
+  /** The array of per-thread data */
+  ThreadData          threadData[];
+};
 
 /**
  * Convert a generic VDOCompletion to a ReadOnlyNotifier.
