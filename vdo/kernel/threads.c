f7bcfbd75cbb5b68585be16418767afb565f8df5
Red Hat VDO Team <vdo-devel@redhat.com>
2019-05-13 17:04:00 -0400

    139114 by joe on 2019/05/13 (pair: jshimkus) Reindent threads.c (VDOUP-6).
    The identifier getThreadId will be decamelcased later.
    
    .../threads.c
    Reindent according to kernel standards.
    
    Co-authored-by: joe <jochapma@redhat.com>
    Co-authored-by: jshimkus <jshimkus@redhat.com>

--- vdo/kernel/threads.c
+++ vdo/kernel/threads.c
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301, USA. 
  *
- * $Id: //eng/linux-vdo/src/c++/vdo/kernel/threads.c#1 $
+ * $Id: //eng/linux-vdo/src/c++/vdo/kernel/threads.c#2 $
  */
 
 #include "threads.h"
@@ -27,5 +27,5 @@
 /**********************************************************************/
 pid_t getThreadId(void)
 {
-  return in_interrupt() ? -1 : current->pid;
+	return in_interrupt() ? -1 : current->pid;
 }
