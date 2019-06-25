5df0f1e7cc7fc4f72803913e9792e44e35813e50
Red Hat VDO Team <vdo-devel@redhat.com>
2019-06-24 22:04:43 -0400

    139772 by bjohnston on 2019/06/24 (pair: sclafani) [VDO-4661] [BZ 1643291] Merge 139631 to linux-vdo. 139631 by bjohnston on 2019/06/12 (pair: msakai) [VDO-4461] [BZ 1643291] Decide how to close index based on version.
    .../dmvdo.c
    Update target version for index-close message addition.
    
    Co-authored-by: bjohnston <bjohnsto@redhat.com>
    Co-authored-by: sclafani <sclafani@redhat.com>
    Co-authored-by: msakai <msakai@redhat.com>

--- vdo/kernel/dmvdo.c
+++ vdo/kernel/dmvdo.c
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301, USA. 
  *
- * $Id: //eng/linux-vdo/src/c++/vdo/kernel/dmvdo.c#30 $
+ * $Id: //eng/linux-vdo/src/c++/vdo/kernel/dmvdo.c#31 $
  */
 
 #include "dmvdo.h"
@@ -814,7 +814,7 @@ static void vdo_resume(struct dm_target *ti)
  */
 static struct target_type vdo_target_bio = {
 	.name = "vdo",
-	.version = { 6, 2, 0 },
+	.version = { 6, 2, 1 },
 	.module = THIS_MODULE,
 	.ctr = vdo_ctr,
 	.dtr = vdo_dtr,
