bff386a50617afa4a03068f613e6ddf012232340
Red Hat VDO Team <vdo-devel@redhat.com>
2020-01-21 20:54:24 -0500

    142182 by corwin on 2020/01/21 (pair: sweettea ) Merged 140311 to linux-vdo. 140311 by awalsh on 2019/08/09 (pair: corwin, sclafani, sweettea) Updated Github README to show releases
    We often get bug reports with people asking for VDO to work with
    random kernels.  Adding a release table will hopefully help set the
    expectations on where things should work, and where they may need some
    attention when using a different distro.
    
    .../README.md
    Added releases to the README files
    
    Co-authored-by: corwin <corwin@redhat.com>
    Co-authored-by: sweettea <sweettea@redhat.com>
    Co-authored-by: awalsh <awalsh@redhat.com>
    Co-authored-by: sclafani <sclafani@redhat.com>

--- vdo/kernel/dataKVIO.c
+++ vdo/kernel/dataKVIO.c
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301, USA. 
  *
- * $Id: //eng/linux-vdo/src/c++/vdo/kernel/dataKVIO.c#44 $
+ * $Id: //eng/linux-vdo/src/c++/vdo/kernel/dataKVIO.c#45 $
  */
 
 #include "dataKVIO.h"
--- vdo/kernel/dataKVIO.h
+++ vdo/kernel/dataKVIO.h
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301, USA. 
  *
- * $Id: //eng/linux-vdo/src/c++/vdo/kernel/dataKVIO.h#26 $
+ * $Id: //eng/linux-vdo/src/c++/vdo/kernel/dataKVIO.h#27 $
  */
 
 #ifndef DATA_KVIO_H
