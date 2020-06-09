3460abac0d3925f8a6a2efa98e4ca5dfb671dcbb
Red Hat VDO Team <vdo-devel@redhat.com>
2020-06-09 05:31:07 -0400

    145054 by joe on 2020/06/09 (pair: jshimkus) Remove unused ringNode.h include (VDOUP-7).
    	.../dmvdo.c
    	Remove unused include.
    
    Co-authored-by: jochapma@redhat.com
    Co-authored-by: jshimkus@redhat.com

--- vdo/kernel/dmvdo.c
+++ vdo/kernel/dmvdo.c
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301, USA. 
  *
- * $Id: //eng/linux-vdo/src/c++/vdo/kernel/dmvdo.c#56 $
+ * $Id: //eng/linux-vdo/src/c++/vdo/kernel/dmvdo.c#57 $
  */
 
 #include "dmvdo.h"
@@ -27,7 +27,6 @@
 #include "memoryAlloc.h"
 
 #include "constants.h"
-#include "ringNode.h"
 #include "threadConfig.h"
 #include "vdo.h"
 
