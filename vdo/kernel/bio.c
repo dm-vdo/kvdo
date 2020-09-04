dc810912bf30e3240526b886ad87358550b18f83
Red Hat VDO Team <vdo-devel@redhat.com>
2020-09-04 07:41:01 -0400

    146851 by sweettea on 2020/09/04 (pair: joe) Clarify and improve set_bio_operation()
    	A bio operation (e.g. REQ_OP_DISCARD, READ) is essentially
    	a member of an enum, and is stored in the lower few bits of
    	the bio's bi_opf field. (bi_opf & REQ_OP_MASK) is the
    	operation, while the remaining bits are various flags. In
    	particular, the operations are not flags and should not be
    	treated as such; to preserve flags while setting bi_opf,
    	one should do (bi_opf & ~REQ_OP_MASK) | (new_op) in order
    	to be sure to clear out the old operation.
    
    	The code in set_bio_operation was clearing the flags of
    	bi_opf, then setting the operation as though it were a flag.
    	It is clearer to replace it with a straight setting of
    	bi_opf to the correct value, and avoids the risk of leaving
    	bits of the previous operation behind.
    
    	.../bio.c
    	Simplified and improved set_bio_operation().
    
    Co-authored-by: sweettea@redhat.com
    Co-authored-by: jochapma@redhat.com

--- vdo/kernel/bio.c
+++ vdo/kernel/bio.c
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301, USA. 
  *
- * $Id: //eng/linux-vdo/src/c++/vdo/kernel/bio.c#27 $
+ * $Id: //eng/linux-vdo/src/c++/vdo/kernel/bio.c#28 $
  */
 
 #include "bio.h"
@@ -66,8 +66,7 @@ void bio_copy_data_out(struct bio *bio, char *data_ptr)
 /**********************************************************************/
 void set_bio_operation(struct bio *bio, unsigned int operation)
 {
-	bio->bi_opf &= ~REQ_OP_MASK;
-	bio->bi_opf |= operation;
+	bio->bi_opf = operation;
 }
 
 /**********************************************************************/
