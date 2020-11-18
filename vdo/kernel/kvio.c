f435dfccb1362fbbadc33f8470fbb33ae08189e0
Red Hat VDO Team <vdo-devel@redhat.com>
2020-11-18 08:15:31 -0500

    147691 by sweettea on 2020/11/18 (pair: joe) Remove extraneous bio field editing by the kvio.
    	.../kvio.c
    	Removed setting of the kvio's bio's bi_private field at initialization;
    	  the bio will be fully set up, if appropriate, when the kvio decides
    	  to use it for IO.
    
    Co-authored-by: sweettea@redhat.com
    Co-authored-by: jochapma@redhat.com

--- vdo/kernel/kvio.c
+++ vdo/kernel/kvio.c
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301, USA. 
  *
- * $Id: //eng/linux-vdo/src/c++/vdo/kernel/kvio.c#50 $
+ * $Id: //eng/linux-vdo/src/c++/vdo/kernel/kvio.c#51 $
  */
 
 #include "kvio.h"
@@ -292,9 +292,6 @@ void initialize_kvio(struct kvio *kvio,
 
 	kvio->bio = bio;
 	kvio->layer = layer;
-	if (bio != NULL) {
-		bio->bi_private = kvio;
-	}
 
 	initialize_vio(kvio->vio,
 		       vio_type,
