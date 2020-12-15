c2bdbf7cbb2db36e1a5fa712992a07d5a9ba0399
Red Hat VDO Team <vdo-devel@redhat.com>
2020-12-15 04:44:46 -0500

    147957 by sclafani on 2020/12/15 (pair: joe) Fixed kvdo_flush declaration in kernelTypes.h [VDOUP-6]
    	I don't know why we can build without some forward declarations but
    	not others, but it seems better to have it correctly declared than
    	to remove it.
    
    	.../kernelTypes.h
    	Renamed forward declaration of opaque struct kvdoFlush to kvdo_flush.
    
    Co-authored-by: sclafani@redhat.com
    Co-authored-by: jochapma@redhat.com

--- vdo/kernelTypes.h
+++ vdo/kernelTypes.h
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301, USA. 
  *
- * $Id: //eng/linux-vdo/src/c++/vdo/kernel/kernelTypes.h#21 $
+ * $Id: //eng/linux-vdo/src/c++/vdo/kernel/kernelTypes.h#22 $
  */
 
 #ifndef KERNEL_TYPES_H
@@ -30,7 +30,7 @@ struct dedupe_index;
 struct io_submitter;
 struct kernel_layer;
 struct kvdo;
-struct kvdoFlush;
+struct kvdo_flush;
 struct vdo_work_item;
 struct vdo_work_queue;
 
