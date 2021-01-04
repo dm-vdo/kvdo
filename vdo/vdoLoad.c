1322ff9c8ae9d335d6831a157fe4163224445d38
Red Hat VDO Team <vdo-devel@redhat.com>
2021-01-04 18:48:44 -0500

    148109 by sclafani on 2021/01/04 (pair: corwin) Fixed a reference to AdminCompletion in a comment [VDOUP-6]
    	.../vdoLoad.c
    	Replaced AdminCompletion with admin_completion in a comment.
    
    Co-authored-by: sclafani@redhat.com
    Co-authored-by: corwin@redhat.com

--- vdo/vdoLoad.c
+++ vdo/vdoLoad.c
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301, USA. 
  *
- * $Id: //eng/linux-vdo/src/c++/vdo/base/vdoLoad.c#57 $
+ * $Id: //eng/linux-vdo/src/c++/vdo/base/vdoLoad.c#58 $
  */
 
 #include "vdoLoad.h"
@@ -44,10 +44,10 @@
 #include "vdoRecovery.h"
 
 /**
- * Extract the vdo from an AdminCompletion, checking that the current operation
- * is a load.
+ * Extract the vdo from an admin_completion, checking that the current
+ * operation is a load.
  *
- * @param completion  The AdminCompletion's sub-task completion
+ * @param completion  The admin_completion's sub-task completion
  *
  * @return The vdo
  **/
