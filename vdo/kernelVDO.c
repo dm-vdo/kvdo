8dee083eacae042a7b2ccaded2b41450943ff5e4
Red Hat VDO Team <vdo-devel@redhat.com>
2021-01-19 19:53:46 -0500

    148315 by sweettea on 2021/01/19 (pair: corwin) Removed include of vdoDebug.h which was removed in 148306.
    	.../kernelVDO.c
    	Removed include of vdoDebug.h.
    
    Co-authored-by: sweettea@redhat.com
    Co-authored-by: corwin@redhat.com

--- vdo/kernelVDO.c
+++ vdo/kernelVDO.c
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301, USA. 
  *
- * $Id: //eng/linux-vdo/src/c++/vdo/kernel/kernelVDO.c#75 $
+ * $Id: //eng/linux-vdo/src/c++/vdo/kernel/kernelVDO.c#76 $
  */
 
 /*
@@ -35,7 +35,6 @@
 #include "statistics.h"
 #include "threadConfig.h"
 #include "vdo.h"
-#include "vdoDebug.h"
 //#include "vdoInternal.h"
 #include "vdoLoad.h"
 #include "vdoResize.h"
