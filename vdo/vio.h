560b06b4b0572c6604098f09c6c68a5232fa9241
Red Hat VDO Team <vdo-devel@redhat.com>
2021-06-08 03:27:13 -0400

    150145 by jshimkus on 2021/06/08 (pair: sclafani) [VDOUP-6] Disambiguate ktrace.[hc] functions.
    	.../vio.h
    	Adapted to ktrace.[hc] disambiguation.
    
    Co-authored-by: jshimkus@redhat.com
    Co-authored-by: sclafani@redhat.com

--- vdo/vio.h
+++ vdo/vio.h
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301, USA. 
  *
- * $Id: //eng/linux-vdo/src/c++/vdo/base/vio.h#50 $
+ * $Id: //eng/linux-vdo/src/c++/vdo/base/vio.h#51 $
  */
 
 #ifndef VIO_H
