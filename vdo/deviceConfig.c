4f2eb137016e83652bba4717f37507a870a13f60
Red Hat VDO Team <vdo-devel@redhat.com>
2021-08-11 16:39:58 -0400

    151879 by corwin on 2021/08/11 (pair: sclafani) Moved deviceConfig from kernel to base [VDOUP-3].
    	As a prequel to making prepare_to_modify_kernel_layer() not rely on the
    	layer, this change facilitates simplifying and cleaning up error
    	checking in the growth processes by allowing unit tests to test
    	validation of the device config directly.
    
    	.../kernel/deviceConfig.h
    	.../kernel/deviceConfig.c
    	.../base/deviceConfig.h
    	.../base/deviceConfig.c
    	Moved deviceConfig.[hc] from kernel to base. The table parsing is
    	  ifdefed __KERNEL__.
    
    	.../base/deviceConfig.c
    	Moved the guts of freeDeviceConfig() into free_vdo_device_config()
    	  (ifdefed not __KERNEL__). Replaced all calls to the former with the
    	  latter and removed freeDeviceConfig().
    
    Co-authored-by: corwin@redhat.com
    Co-authored-by: sclafani@redhat.com

--- vdo/deviceConfig.c
+++ vdo/deviceConfig.c
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301, USA. 
  *
- * $Id: //eng/linux-vdo/src/c++/vdo/kernel/deviceConfig.c#43 $
+ * $Id: //eng/linux-vdo/src/c++/vdo/base/deviceConfig.c#1 $
  */
 
 #include "deviceConfig.h"
--- vdo/deviceConfig.h
+++ vdo/deviceConfig.h
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301, USA. 
  *
- * $Id: //eng/linux-vdo/src/c++/vdo/kernel/deviceConfig.h#27 $
+ * $Id: //eng/linux-vdo/src/c++/vdo/base/deviceConfig.h#1 $
  */
 #ifndef DEVICE_CONFIG_H
 #define DEVICE_CONFIG_H
