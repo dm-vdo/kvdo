c4b40f05ddfed90c05a95e2e4e4e287416a5389a
Red Hat VDO Team <vdo-devel@redhat.com>
2021-08-19 14:19:18 -0400

    152019 by sweettea on 2021/08/19 (pair: sclafani) Delete vdoRecoveryInternals.h, not in use by tests.
    	When first created, recovery could be tested without a full VDO.
    	This is no longer true, and the functions and structures in the
    	internals header are no longer used.
    
    	.../vdoRecoveryInternals.h
    	.../vdoRecovery.c
    	Move contents to vdoRecovery.c.
    	Adjust static designators, and move free() functionality to above
    	  make() since there is no forward declaration.
    
    Co-authored-by: sweettea@redhat.com
    Co-authored-by: sclafani@redhat.com

--- vdo/vdoRecovery.c
+++ vdo/vdoRecovery.c
@@ -16,10 +16,10 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301, USA. 
  *
- * $Id: //eng/linux-vdo/src/c++/vdo/base/vdoRecovery.c#112 $
+ * $Id: //eng/linux-vdo/src/c++/vdo/base/vdoRecovery.c#113 $
  */
 
-#include "vdoRecoveryInternals.h"
+#include "vdoRecovery.h"
 
 #include "logger.h"
 #include "memoryAlloc.h"
@@ -31,6 +31,8 @@
 #include "blockMapPage.h"
 #include "blockMapRecovery.h"
 #include "completion.h"
+#include "intMap.h"
+#include "journalPoint.h"
 #include "numUtils.h"
 #include "packedRecoveryJournalBlock.h"
 #include "recoveryJournal.h"
@@ -40,8 +42,83 @@
 #include "slabJournal.h"
 #include "slabJournalInternals.h"
 #include "threadConfig.h"
+#include "types.h"
 #include "vdoInternal.h"
 #include "waitQueue.h"
+
+/**
+ * The absolute position of an entry in the recovery journal, including
+ * the sector number and the entry number within the sector.
+ **/
+struct recovery_point {
+	sequence_number_t sequence_number; // Block sequence number
+	uint8_t sector_count; // Sector number
+	journal_entry_count_t entry_count; // Entry number
+};
+
+struct recovery_completion {
+	/** The completion header */
+	struct vdo_completion completion;
+	/** The sub-task completion */
+	struct vdo_completion sub_task_completion;
+	/** The struct vdo in question */
+	struct vdo *vdo;
+	/** The struct block_allocator whose journals are being recovered */
+	struct block_allocator *allocator;
+	/** A buffer to hold the data read off disk */
+	char *journal_data;
+	/** The number of increfs */
+	size_t incref_count;
+
+	/** The entry data for the block map recovery */
+	struct numbered_block_mapping *entries;
+	/** The number of entries in the entry array */
+	size_t entry_count;
+	/**
+	 * The sequence number of the first valid block for block map recovery
+	 */
+	sequence_number_t block_map_head;
+	/**
+	 * The sequence number of the first valid block for slab journal replay
+	 */
+	sequence_number_t slab_journal_head;
+	/**
+	 * The sequence number of the last valid block of the journal (if
+	 * known)
+	 */
+	sequence_number_t tail;
+	/**
+	 * The highest sequence number of the journal, not the same as the tail,
+	 * since the tail ignores blocks after the first hole.
+	 */
+	sequence_number_t highest_tail;
+
+	/** A location just beyond the last valid entry of the journal */
+	struct recovery_point tail_recovery_point;
+	/** The location of the next recovery journal entry to apply */
+	struct recovery_point next_recovery_point;
+	/** The number of logical blocks currently known to be in use */
+	block_count_t logical_blocks_used;
+	/** The number of block map data blocks known to be allocated */
+	block_count_t block_map_data_blocks;
+	/** The journal point to give to the next synthesized decref */
+	struct journal_point next_journal_point;
+	/** The number of entries played into slab journals */
+	size_t entries_added_to_slab_journals;
+
+	// Decref synthesis fields
+
+	/** An int_map for use in finding which slots are missing decrefs */
+	struct int_map *slot_entry_map;
+	/** The number of synthesized decrefs */
+	size_t missing_decref_count;
+	/** The number of incomplete decrefs */
+	size_t incomplete_decref_count;
+	/** The fake journal point of the next missing decref */
+	struct journal_point next_synthesized_journal_point;
+	/** The queue of missing decrefs */
+	struct wait_queue missing_decrefs[];
+};
 
 enum {
 	// The int map needs capacity of twice the number of VIOs in the system.
@@ -103,6 +180,20 @@ static int enqueue_missing_decref(struct wait_queue *queue,
 	}
 
 	return result;
+}
+
+/**
+ * Convert a generic completion to a recovery_completion.
+ *
+ * @param completion  The completion to convert
+ *
+ * @return The recovery_completion
+ **/
+static inline struct recovery_completion * __must_check
+as_vdo_recovery_completion(struct vdo_completion *completion)
+{
+	assert_vdo_completion_type(completion->type, VDO_RECOVERY_COMPLETION);
+	return container_of(completion, struct recovery_completion, completion);
 }
 
 /**
@@ -293,8 +384,53 @@ static void prepare_sub_task(struct recovery_completion *recovery,
 			       &recovery->completion);
 }
 
-/**********************************************************************/
-int make_vdo_recovery_completion(struct vdo *vdo,
+/**
+ * A waiter callback to free missing_decrefs.
+ *
+ * Implements waiter_callback.
+ **/
+static void free_missing_decref(struct waiter *waiter,
+				void *context __always_unused)
+{
+	UDS_FREE(as_missing_decref(waiter));
+}
+
+/**
+ * Free a recovery_completion and all underlying structures.
+ *
+ * @param recovery  The recovery completion to free
+ **/
+static void free_vdo_recovery_completion(struct recovery_completion *recovery)
+{
+	const struct thread_config *thread_config;
+	zone_count_t z;
+
+	if (recovery == NULL) {
+		return;
+	}
+
+	free_int_map(UDS_FORGET(recovery->slot_entry_map));
+	thread_config = get_vdo_thread_config(recovery->vdo);
+	for (z = 0; z < thread_config->physical_zone_count; z++) {
+		notify_all_waiters(&recovery->missing_decrefs[z],
+				   free_missing_decref, NULL);
+	}
+
+	UDS_FREE(UDS_FORGET(recovery->journal_data));
+	UDS_FREE(UDS_FORGET(recovery->entries));
+	UDS_FREE(recovery);
+}
+
+/**
+ * Allocate and initialize a recovery_completion.
+ *
+ * @param vdo           The vdo in question
+ * @param recovery_ptr  A pointer to hold the new recovery_completion
+ *
+ * @return VDO_SUCCESS or a status code
+ **/
+__must_check
+static int make_vdo_recovery_completion(struct vdo *vdo,
 				 struct recovery_completion **recovery_ptr)
 {
 	const struct thread_config *thread_config = get_vdo_thread_config(vdo);
@@ -327,39 +463,6 @@ int make_vdo_recovery_completion(struct vdo *vdo,
 
 	*recovery_ptr = recovery;
 	return VDO_SUCCESS;
-}
-
-/**
- * A waiter callback to free missing_decrefs.
- *
- * Implements waiter_callback.
- **/
-static void free_missing_decref(struct waiter *waiter,
-				void *context __always_unused)
-{
-	UDS_FREE(as_missing_decref(waiter));
-}
-
-/**********************************************************************/
-void free_vdo_recovery_completion(struct recovery_completion *recovery)
-{
-	const struct thread_config *thread_config;
-	zone_count_t z;
-
-	if (recovery == NULL) {
-		return;
-	}
-
-	free_int_map(UDS_FORGET(recovery->slot_entry_map));
-	thread_config = get_vdo_thread_config(recovery->vdo);
-	for (z = 0; z < thread_config->physical_zone_count; z++) {
-		notify_all_waiters(&recovery->missing_decrefs[z],
-				   free_missing_decref, NULL);
-	}
-
-	UDS_FREE(UDS_FORGET(recovery->journal_data));
-	UDS_FREE(UDS_FORGET(recovery->entries));
-	UDS_FREE(recovery);
 }
 
 /**
