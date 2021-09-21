d50968563e9c4db9981e82bd07be5aeab46c4fde
Red Hat VDO Team <vdo-devel@redhat.com>
2021-09-20 21:03:19 -0400

    152453 by corwin on 2021/09/20 (pair: raeburn) Fixed typo in change 152451.
    	.../vio.h
    	Removed extraneous newline in linux/kernel.h include.
    
    Co-authored-by: corwin@redhat.com
    Co-authored-by: raeburn@redhat.com

--- kvdo.spec
+++ kvdo.spec
@@ -96,5 +96,5 @@ rm -rf $RPM_BUILD_ROOT
 %{_usr}/src/%{kmod_name}-%{version}
 
 %changelog
-* Sat Sep 18 2021 - Red Hat VDO Team <vdo-devel@redhat.com> - 8.2.0.0-1
+* Mon Sep 20 2021 - Red Hat VDO Team <vdo-devel@redhat.com> - 8.2.0.0-1
 - See https://github.com/dm-vdo/kvdo.git
--- vdo/vio.h
+++ vdo/vio.h
@@ -16,14 +16,14 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301, USA. 
  *
- * $Id: //eng/linux-vdo/src/c++/vdo/base/vio.h#57 $
+ * $Id: //eng/linux-vdo/src/c++/vdo/base/vio.h#58 $
  */
 
 #ifndef VIO_H
 #define VIO_H
 
-#include <linux/kernel.h
->
+#include <linux/kernel.h>
+
 #include "bio.h"
 #include "completion.h"
 #include "kernelTypes.h"
